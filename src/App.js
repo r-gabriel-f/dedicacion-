014235baffc2ab860607e034648ac505c5fae833
robert <r.gabriel.f@outlook.com>
2023-12-19 16:56:34 -0400

    se instalo tailwind

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
