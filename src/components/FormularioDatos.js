c7c0ab6cc548a1f2fbf07017f11d439a9304eda1
robert <r.gabriel.f@outlook.com>
2023-12-28 11:04:30 -0400

    se cambio musica

--- src/components/FormularioDatos.js
+++ src/components/FormularioDatos.js
@@ -3,29 +3,53 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { format } from "date-fns";
 import Swal from "sweetalert2";
-import yo from "../imagene/yo.webp";
-import error from "../imagene/error.webp";
+import yo from "../assets/imagene/yo.webp";
+import error from "../assets/imagene/error.webp";
+import creo from "../assets/musicas/creo.weba";
+import er from "../assets/musicas/error.weba";
 export const FormularioDatos = () => {
   const [fechaAniversario, setFechaAniversario] = useState(new Date());
   const [primerNombre, setPrimerNombre] = useState("");
-
   const handleSubmit = () => {
-    const fechaFormateada = format(fechaAniversario, "dd/MM/yyyy");
-
+      const v = creo;
+      const fechaFormateada = format(fechaAniversario, "dd/MM/yyyy");
     if (
       fechaFormateada === "02/11/2023" &&
       primerNombre.toLowerCase() === "madai"
     ) {
+      const audio = new Audio(v);
+      audio.play();
       Swal.fire({
         title: "TE AMO :)",
-        text: "♥Querida Madai Alejandra Monrroy Vega, En el jardín de mi corazón, florece una flor única y hermosa: tú. Cada día a tu lado es un regalo que atesoro con gratitud y amor. Tu luz ilumina mi vida de maneras que nunca imaginé, y cada momento contigo es un recordatorio de la suerte que tengo de tenerte a mi lado.        Eres la melodía que alegra mis días y la calma que serena mis noches. Tu sonrisa es mi faro en los días nublados, y tus abrazos son el refugio donde encuentro paz y felicidad. Cada pequeño detalle de ti me enamora un poco más, y estoy agradecido por cada instante que compartimos juntos.  Prometo amarte con todo mi ser, con paciencia en los desafíos y alegría en los triunfos. Eres mi inspiración, mi confidente y mi mejor amiga. En este viaje llamado vida, estoy agradecido de tenerte como mi compañera de ruta. Gracias por ser mi razón de sonreír, por llenar mi mundo de amor y por ser la razón por la cual mi corazón late con tanta fuerza. Te amo más de lo que las palabras pueden expresar, y mi deseo más profundo es hacerte feliz todos los días.  Con todo mi amor,   Robert Gabriel Flores bebecita brrrrrr♥",
+        html: `
+        <div>
+
+          <p>♥ Querida Madai Alejandra Monrroy Vega,</p>
+          <p>En el jardín de mi corazón, florece una flor única y hermosa: tú. Cada día a tu lado es un regalo que atesoro con gratitud y amor.</p>
+          <p>Tu luz ilumina mi vida de maneras que nunca imaginé, y cada momento contigo es un recordatorio de la suerte que tengo de tenerte a mi lado.</p>
+          <p>Eres la melodía que alegra mis días y la calma que serena mis noches. Tu sonrisa es mi faro en los días nublados, y tus abrazos son el refugio donde encuentro paz y felicidad.</p>
+          <p>Cada pequeño detalle de ti me enamora un poco más, y estoy agradecido por cada instante que compartimos juntos.</p>
+          <p>Prometo amarte con todo mi ser, con paciencia en los desafíos y alegría en los triunfos. Eres mi inspiración, mi confidente y mi mejor amiga.</p>
+          <p>En este viaje llamado vida, estoy agradecido de tenerte como mi compañera de ruta. Gracias por ser mi razón de sonreír, por llenar mi mundo de amor y por ser la razón por la cual mi corazón late con tanta fuerza.</p>
+          <p>Te amo más de lo que las palabras pueden expresar, y mi deseo más profundo es hacerte feliz todos los días.</p>
+          <p>Con todo mi amor,</p>
+          <p>Robert Gabriel Flores bebecita brrrrrr♥</p>
+        </div>
+      `,
+
         imageUrl: yo,
         imageWidth: 300,
         imageHeight: 200,
         imageAlt: "Custom image",
         confirmButtonText: "Mi Reyna",
+        onClose: () => {
+          audio.pause();
+          audio.currentTime = 0;
+        },
       });
     } else {
+      const audio = new Audio(er);
+      audio.play();
       Swal.fire({
         title: "LO OLVIDASTE :(",
         text: "Quien sera el otro noooooo jajajaaj :v",
